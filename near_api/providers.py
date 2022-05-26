69357857b59aa8691b473e7cef705533cb1ae49a
javadhaqani <javad.haqani@gmail.com>
2022-05-26 11:39:49 +0430

    better typing

--- near_api/providers.py
+++ near_api/providers.py
@@ -1,6 +1,15 @@
 import requests
 import base64
 import json
+from typing import Union, Tuple
+
+TimeoutType = Union[float, Tuple[float, float]]
+''' The type used as "timeout" argument when sending requests. Quantities are in seconds.
+As a float, it indicates how long to wait for the server to send data,
+As a (connect timeout, read timeout) tuple, it specifically indicates how long to
+wait for the connection to establish and how long to wait for the sever to respond.
+See https://requests.readthedocs.io/en/latest/api/#requests.request
+'''
 
 class FinalityTypes():
     FINAL = 'final'
@@ -20,7 +29,7 @@ class JsonProvider(object):
     def rpc_addr(self) -> str:
         return self._rpc_addr
 
-    def json_rpc(self, method: str, params, timeout=2) -> dict:
+    def json_rpc(self, method: str, params, timeout: TimeoutType=2.0) -> dict:
         j = {
             'method': method,
             'params': params,
@@ -34,27 +43,27 @@ class JsonProvider(object):
             raise JsonProviderError(content["error"])
         return content["result"]
 
-    def send_tx(self, signed_tx: bytes, timeout: int=2) -> dict:
+    def send_tx(self, signed_tx: bytes, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('broadcast_tx_async',
                              [base64.b64encode(signed_tx).decode('utf8')], timeout=timeout)
 
-    def send_tx_and_wait(self, signed_tx: bytes, timeout: int) -> dict:
+    def send_tx_and_wait(self, signed_tx: bytes, timeout: TimeoutType) -> dict:
         return self.json_rpc('broadcast_tx_commit',
                              [base64.b64encode(signed_tx).decode('utf8')],
                              timeout=timeout)
 
-    def get_status(self) -> dict:
-        r = requests.get("%s/status" % self.rpc_addr(), timeout=2)
+    def get_status(self, timeout: TimeoutType=2.0) -> dict:
+        r = requests.get("%s/status" % self.rpc_addr(), timeout=timeout)
         r.raise_for_status()
         return json.loads(r.content)
 
-    def get_validators(self, timeout: int=2) -> dict:
+    def get_validators(self, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('validators', [None], timeout=timeout)
 
-    def query(self, query_object, timeout: int=2) -> dict:
+    def query(self, query_object, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('query', query_object, timeout=timeout)
 
-    def get_account(self, account_id: str, finality=FinalityTypes.OPTIMISTIC, timeout: int=2) -> dict:
+    def get_account(self, account_id: str, finality=FinalityTypes.OPTIMISTIC, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc(
             'query', {
                 "request_type": "view_account",
@@ -62,7 +71,7 @@ class JsonProvider(object):
                 "finality": finality
             }, timeout=timeout)
 
-    def get_access_key_list(self, account_id: str, finality=FinalityTypes.OPTIMISTIC, timeout: int=2) -> dict:
+    def get_access_key_list(self, account_id: str, finality=FinalityTypes.OPTIMISTIC, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc(
             'query', {
                 "request_type": "view_access_key_list",
@@ -70,7 +79,7 @@ class JsonProvider(object):
                 "finality": finality
             }, timeout=timeout)
 
-    def get_access_key(self, account_id: str, public_key: str, finality=FinalityTypes.OPTIMISTIC, timeout: int=2) -> dict:
+    def get_access_key(self, account_id: str, public_key: str, finality=FinalityTypes.OPTIMISTIC, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc(
             'query', {
                 "request_type": "view_access_key",
@@ -79,7 +88,7 @@ class JsonProvider(object):
                 "finality": finality
             }, timeout=timeout)
 
-    def view_call(self, account_id: str, method_name: str, args: bytes, finality=FinalityTypes.OPTIMISTIC, timeout: int=2):
+    def view_call(self, account_id: str, method_name: str, args: bytes, finality=FinalityTypes.OPTIMISTIC, timeout: TimeoutType=2.0):
         return self.json_rpc(
             'query', {
                 "request_type": "call_function",
@@ -89,16 +98,16 @@ class JsonProvider(object):
                 "finality": finality
             }, timeout=timeout)
 
-    def get_block(self, block_id, timeout: int=2) -> dict:
+    def get_block(self, block_id, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('block', [block_id], timeout=timeout)
 
-    def get_chunk(self, chunk_id, timeout: int=2) -> dict:
+    def get_chunk(self, chunk_id, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('chunk', [chunk_id], timeout=timeout)
 
-    def get_tx(self, tx_hash, tx_recipient_id, timeout: int=2) -> dict:
+    def get_tx(self, tx_hash, tx_recipient_id, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('tx', [tx_hash, tx_recipient_id], timeout=timeout)
 
-    def get_changes_in_block(self, block_id=None, finality:str=None, timeout: int=2) -> dict:
+    def get_changes_in_block(self, block_id=None, finality:str=None, timeout: TimeoutType=2.0) -> dict:
         '''Use either block_id or finality. Choose finality from "finality_types" class'''
         params = {}
         if block_id:
@@ -131,5 +140,5 @@ class JsonProvider(object):
     def get_next_light_client_block(self, last_block_hash) -> dict:
         return self.json_rpc('next_light_client_block', [last_block_hash])
 
-    def get_receipt(self, receipt_hash, timeout: int=2) -> dict:
+    def get_receipt(self, receipt_hash, timeout: TimeoutType=2.0) -> dict:
         return self.json_rpc('EXPERIMENTAL_receipt', [receipt_hash], timeout=timeout)
